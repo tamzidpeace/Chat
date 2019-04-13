f4ea7a67ffb8d2bc12e474c22c57239a58c9a059
tamzidpeace <tamjedpeace@gmail.com>
2019-04-13 16:11:02 +0600

    signout

--- app/src/main/java/com/example/chat/MainActivity.java
+++ app/src/main/java/com/example/chat/MainActivity.java
@@ -1,15 +1,21 @@
 package com.example.chat;
 
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -34,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText textField;
     private Button sendBtn;
     private long childNum;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,6 +50,7 @@ public class MainActivity extends AppCompatActivity {
         myRef = FirebaseDatabase.getInstance().getReference("abc");
         textField = findViewById(R.id.message_field);
         sendBtn = findViewById(R.id.send_btn);
+        mAuth = FirebaseAuth.getInstance();
 
         createMessageList();
         createRecyclerView();
@@ -98,5 +106,25 @@ public class MainActivity extends AppCompatActivity {
                 Log.d(TAG, "onClick: " + num);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.action_signout) {
+            Toast.makeText(this, "Signed out!", Toast.LENGTH_SHORT).show();
+            mAuth.signOut();
+            startActivity(new Intent(MainActivity.this, Signin.class));
+        }
+
+        return true;
     }
 }
